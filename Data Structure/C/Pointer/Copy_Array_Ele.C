ae330c724ad204096d29bb2ab63933a26e24901f
Vaibhav Nagnath Shinde <103619246+Vaibhu18@users.noreply.github.com>
2023-12-28 16:40:45 +0000

    new program

--- Data Structure/C/Pointer/Copy_Array_Ele.C
+++ Data Structure/C/Pointer/Copy_Array_Ele.C
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void copyArrayElements(int *arr, int *newArr, int size);
 int main()
 {
     int size;
@@ -12,7 +13,30 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
-    
+
+    printf("Original Array : ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    copyArrayElements(arr, newArr, size);
+
+    printf("\nAfter Copy : ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", newArr[i]);
+    }
+
     printf("\n");
     return 0;
+}
+
+void copyArrayElements(int *arr, int *newArr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        *(newArr + i) = *(arr + i);
+        // newArr[i] = arr[i];
+    }
 }
